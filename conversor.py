1f119baadb26ca85941ba5fe8cf70db26e1c8fce
Jorge Winder <jorge.winder@gmail.com>
2021-12-26 21:40:22 -0500

    Primera carga

---
+++ conversor.py
@@ -0,0 +1,39 @@
+import os
+import requests
+from datetime import date
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+print("\n========================================")
+print("======== Casa de cambio digital ========")
+print("========================================\n")
+
+ 
+
+print(f'Tipo de cambio SUNAT para hoy {date.today()}\n')
+
+tipo_de_cambio = requests.get(f'https://api.apis.net.pe/v1/tipo-cambio-sunat?fecha={date.today()}').json()
+operación = input('La operación es de ... Compra (c) o Venta (v) =>')
+
+monto = None
+
+while monto is None:
+
+    try:
+        if operación.upper() == 'C':
+            valor = tipo_de_cambio['compra']
+            monto = float(input("Monto en dólares para la compra($) :"))
+            print(f'Monto de cambio a soles :\n{round(monto * valor, 2)} soles peruanos\n')
+        else:
+            valor = tipo_de_cambio['venta']
+            monto = float(input("Monto en soles para la venta(S/) :"))
+            print(f'Monto de cambio a dólares :\n{round(monto * valor, 2)} dólares\n')
+
+            print(f'¡Gracias por hacer su cambio digital!\n')
+
+    except:
+
+        if monto is None:
+            print(f'\n>>> Debe ingresar monto <<<<\n')
+
+input('-')
